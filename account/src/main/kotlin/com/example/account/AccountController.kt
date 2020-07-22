2051cb449d4490a03972568de4d8d42b912903f1
ElinaValieva <veaufa@mail.ru>
2020-07-22 23:58:05 +0300

    Fix: request parameters

--- account/src/main/kotlin/com/example/account/AccountController.kt
+++ account/src/main/kotlin/com/example/account/AccountController.kt
@@ -1,9 +1,11 @@
 package com.example.account
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/account")
 class AccountController {
 
     @GetMapping("/")
--- order/src/main/kotlin/com/example/order/OrderController.kt
+++ order/src/main/kotlin/com/example/order/OrderController.kt
@@ -1,9 +1,11 @@
 package com.example.order
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/order")
 class OrderController {
 
     @GetMapping("/")
