c678318d3db1d660daabc45611f49cf080a110a9
ElinaValieva <veaufa@mail.ru>
2020-07-24 22:44:00 +0300

    Feat: eureka support

--- config/src/main/kotlin/com/example/config/ConfigApplication.kt
+++ config/src/main/kotlin/com/example/config/ConfigApplication.kt
@@ -2,8 +2,10 @@ package com.example.config
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class ConfigApplication
 
 fun main(args: Array<String>) {
--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-zuul")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
